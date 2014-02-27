72bae1208fa7876d7712f51d22f63049296b5c70
Zion <zionorent@gmail.com>
2014-02-27 18:04:06 -0500

    Fix JS bug
    ‘buff said

--- videoControlObjects.js
+++ videoControlObjects.js
@@ -5,7 +5,7 @@ function updateCallStatus(text)
 }
 function trace(text)
 {
-	console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+	console.log((Date.now() / 1000).toFixed(3) + ": " + text);
 }
 function handleSuccess(text)
 {
